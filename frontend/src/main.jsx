4c9aee6c2abbddad44c691a88645491b8c83d9ac
Adarsh Kesharwani <kesharwaniadarsh24@gmail.com>
2025-10-03 23:39:44 +0530

    Add Vercel Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './setupApi.js'; // ensure this runs before components that call /api
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
