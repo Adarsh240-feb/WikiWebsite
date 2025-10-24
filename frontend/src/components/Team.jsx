dcecf55b9e5f84f4f3e8ad328590fac11497fa71
Adarsh Kesharwani <kesharwaniadarsh24@gmail.com>
2025-10-24 13:56:46 +0530

    Changes in Team Page

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -96,8 +96,7 @@ const volunteers = [
   { name: "Neelima Srivastava", affiliation: "Event Coordination & Management", image: Neelima, linkedin: "https://www.linkedin.com/in/neelima-srivastav-b71636370", github: "https://github.com/th-neelimaa" },
   { name: "Suhani Shukla", affiliation: "Visual Design & Branding", image: Suhani, linkedin: "http://www.linkedin.com/in/suhani-shukla-945193285", github: "https://github.com/SUHANI005" },
   { name: "Priyanshika Upadhyay", affiliation: "Event Coordination & Management", image: Priyanshika, linkedin: "https://www.linkedin.com/in/priyanshika-upadhyay-a87a26332?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", github: "https://github.com/Priyanshika1111" },
-
-  { name: "Arohi Singh", affiliation: "Technical Support", image: Arohi, linkedin: "https://www.linkedin.com/in/arohi-singh-549974289/", github: "https://github.com/Arohisngh" },
+  { name: "Arohi Singh", affiliation: "Report Writing & Outreach Lead", image: Arohi, linkedin: "https://www.linkedin.com/in/arohi-singh-549974289/", github: "https://github.com/Arohisngh" },
 ];
 
 const navLinks = [];
